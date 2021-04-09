daa6fb7219622cd3f012c2952503b1aa786f28cb
likanglin <likanglin@bytedance.com>
2021-04-09 19:03:15 +0800

    init
    Change-Id: I299a45eb0d32997e899265582469839f8cef0df1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/15klli/pytest-collect-formatter2',
-    version='0.4.0',
+    version='0.1.0',
     zip_safe=False,
     entry_points={
         'pytest11': [
